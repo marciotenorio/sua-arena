90aef3cf37dd5f66bac72143587e606290bf139a
Márcio Tenório <marciotenorioj@gmail.com>
2022-02-02 22:33:45 -0300

    Initial import

---
+++ src/main/java/br/com/imd/processos/suaarena/SuaArenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.imd.processos.suaarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuaArenaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuaArenaApplication.class, args);
+    }
+
+}
